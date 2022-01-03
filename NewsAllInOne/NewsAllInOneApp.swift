87dd9be7a0e052041f36bc9028cc3e8b89d4188d
emre topcu <emre.topcu1234@gmail.com>
2022-01-03 21:12:39 +0300

    Initial Commit

---
+++ NewsAllInOne/NewsAllInOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAllInOneApp.swift
+//  NewsAllInOne
+//
+//  Created by Emre Topçu on 3.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAllInOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
